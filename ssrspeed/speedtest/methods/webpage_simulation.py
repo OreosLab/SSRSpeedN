cdcf2c107409e2f8427a75588366fc1c559f26f5
Mashiro2000 <2984922017@qq.com>
2022-09-09 21:18:03 +0800

    Update webpage_simulation.py

--- ssrspeed/speedtest/methods/webpage_simulation.py
+++ ssrspeed/speedtest/methods/webpage_simulation.py
@@ -24,8 +24,9 @@ async def start_web_page_simulation_test(local_host, local_port):
     while len(results):
         results.pop()
     task_list = []
+    semaphore = asyncio.Semaphore(w_config.get('maxWorkers',4))
     logger.info("Start web page simulation test.")
-    logger.info("Proxy {}:{}".format(local_host, local_port))
+    logger.info(f"Proxy {local_host}:local_port{local_port}")
     ip_loc = await parse_location(local_port)
     urls = copy.deepcopy(w_config.get("urls", []))
     if ip_loc[0]:
@@ -34,14 +35,14 @@ async def start_web_page_simulation_test(local_host, local_port):
     logger.info("Read {} url(s).".format(len(urls)))
     for url in urls:
         task_list.append(
-            asyncio.create_task(execute(url=url, host=local_host, port=local_port))
+            asyncio.create_task(execute(url=url, host=local_host, port=local_port, semaphore=semaphore))
         )
     await asyncio.wait(task_list)
     return copy.deepcopy(results)
 
 
-async def execute(url, host, port):
-    logger.debug("{} started. Url: {}".format(asyncio.current_task().get_name(), url))
+async def execute(url, host, port, semaphore):
+    logger.debug(f"{asyncio.current_task().get_name()} started. Url: {url}")
     logger.info("Testing Url : {}".format(url))
     res = {"url": url, "retCode": 0, "time": 0}
     try:
@@ -50,19 +51,18 @@ async def execute(url, host, port):
             connector=ProxyConnector(host=host, port=port),
             timeout=aiohttp.ClientTimeout(connect=10),
         ) as session:
-            async with session.get(url) as response:
-                res["retCode"] = response.status
-                stop_time = time.time()
-                res["time"] = stop_time - start_time
-                logger.info(
-                    "Url: {}, time used: {:.2f}s, code: {}.".format(
-                        url, res["time"], res["retCode"]
+            async with semaphore:
+                async with session.get(url) as response:
+                    res["retCode"] = response.status
+                    stop_time = time.time()
+                    res["time"] = stop_time - start_time
+                    logger.info(
+                        f"Url: {url}, time used: {res['time']:.2f}s, code: {res['retCode']}."
                     )
-                )
     except aiohttp.ClientTimeout:
-        logger.error("Url: {} timeout.".format(url))
+        logger.error(f"Url: {url} timeout.")
     except aiohttp.ClientSSLError:
-        logger.error("SSL Error on : {}".format(url))
+        logger.error(f"SSL Error on : {url}")
     except Exception:
         logger.error(f"Unknown Error on : {url}", exc_info=True)
     finally:
