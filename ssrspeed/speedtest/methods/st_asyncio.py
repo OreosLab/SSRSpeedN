ad940cd47aa6f222d51e5893bd86d4134448a518
Mashiro2000 <2984922017@qq.com>
2022-09-09 21:15:54 +0800

    Update st_asyncio.py

--- ssrspeed/speedtest/methods/st_asyncio.py
+++ ssrspeed/speedtest/methods/st_asyncio.py
@@ -5,7 +5,7 @@ import time
 from typing import Union
 
 import aiohttp
-from aiohttp_socks import SocksConnector
+from aiohttp_socks import ProxyConnector
 
 from ssrspeed.config import ssrconfig
 from ssrspeed.utils import ip_loc
@@ -13,7 +13,7 @@ from ssrspeed.utils.rules import DownloadRuleMatch
 
 logger = logging.getLogger("Sub")
 
-WORKERS = ssrconfig["fileDownload"]["maxWorkers"]
+WORKERS = ssrconfig["fileDownload"]["workers"]
 BUFFER = ssrconfig["fileDownload"]["buffer"]
 
 
@@ -79,65 +79,65 @@ class Statistics:
         print(
             "\r["
             + "=" * self._count
-            + "] [{:.2f} MB/s]".format(mb_red / self._time_used)
+            + "] [{:.2f} MB/s]".format(mb_red / self._time_used if self._time_used != 0 else 0)
         )
-        logger.info("Fetched {:.2f} MB in {:.2f}s.".format(mb_red, self._time_used))
+        logger.info(f"Fetched {mb_red:.2f} MB in {self._time_used:.2f}s.")
 
     def _show_progress(self, delta_time: Union[int, float]):
         speed = (self._total_red - self._delta_red) / delta_time
         speed_mb = speed / 1024 / 1024
         self._delta_red = self._total_red
-        # print(
-        #     "\r["
-        #     + "=" * int(self._time_used // delta_time)
-        #     + "> [{:.2f} MB/s]".format(speed_mb),
-        #     end="",
-        # )
         self._count += 1
-        print("\r[" + "=" * self._count + "> [{:.2f} MB/s]".format(speed_mb), end="")
+        print("\r[" + "=" * self._count + f"> [{speed_mb:.2f} MB/s]", end="")
         self._speed_list.append(speed)
 
 
-async def _fetch(url: str, sta: Statistics, host: str = "127.0.0.1", port: int = 1087):
-    connector = SocksConnector(host=host, port=port, rdns=True)
-    logger.info(f"Fetching {url} via {host}:{port}.")
-    async with aiohttp.ClientSession(
-        connector=connector, headers={"User-Agent": "curl/11.45.14"}
-    ) as session:
-        logger.debug("Session created.")
-        async with session.get(url) as response:
-            logger.debug("Awaiting response.")
-            while not sta.stopped:
-                chunk = await response.content.read(BUFFER)
-                if not chunk:
-                    logger.info("No chunk, task stopped.")
-                    break
-                await sta.record(len(chunk))
+async def _fetch(url: str, sta: Statistics, host: str = "127.0.0.1", port: int = 10870):
+    try:
+        logger.info(f"Fetching {url} via {host}:{port}.")
+        async with aiohttp.ClientSession(
+                headers={"User-Agent": "curl/11.45.14"},
+                connector=ProxyConnector(host=host, port=port),
+                timeout=aiohttp.ClientTimeout(connect=10),
+        ) as session:
+            logger.debug("Session created.")
+            async with session.get(url) as response:
+                logger.debug("Awaiting response.")
+                while not sta.stopped:
+                    chunk = await response.content.read(BUFFER)
+                    if not chunk:
+                        logger.info("No chunk, task stopped.")
+                        break
+                    await sta.record(len(chunk))
+    except Exception as e:
+        logger.error("Connect to Download link exception: " + str(e))
 
 
-def start(
-    proxy_host: str = "127.0.0.1", proxy_port: int = 1087, workers: int = WORKERS
+async def start(
+    download_semaphore, proxy_host: str = "127.0.0.1", proxy_port: int = 10870, workers: int = WORKERS
 ) -> tuple:
-    dlrm = DownloadRuleMatch()
-    res = dlrm.get_url(ip_loc())
-    url = res[0]
-    file_size = res[1]
-    logger.debug(f"Url: {url}, file_size: {file_size} MiB.")
-    logger.info(f"Running st_async, workers: {workers}.")
-    loop = asyncio.new_event_loop()
-    tasks = []
-    _sta = Statistics()
-    for _ in range(0, workers):
-        tasks.append(loop.create_task(_fetch(url, _sta, proxy_host, proxy_port)))
-    loop.run_until_complete(asyncio.wait(tasks))
-    loop.close()
-    _sta.show_progress_full()
-    if _sta.time_used:
-        return (
-            _sta.total_red / _sta.time_used,
-            _sta.max_speed,
-            _sta.speed_list,
-            _sta.total_red,
-        )
-    else:
-        return 0, 0, [], 0
+    async with download_semaphore:
+        dlrm = DownloadRuleMatch()
+        res = dlrm.get_url(await ip_loc(proxy_port))
+        url = res[0]
+        file_size = res[1]
+        logger.debug(f"Url: {url}, file_size: {file_size} MiB.")
+        logger.info(f"Running st_async, workers: {workers}.")
+        _sta = Statistics()
+        tasks = [
+            asyncio.create_task(
+                _fetch(url, _sta, proxy_host, proxy_port)
+            )
+            for _ in range(workers)
+        ]
+        await asyncio.wait(tasks)
+        _sta.show_progress_full()
+        if _sta.time_used:
+            return (
+                _sta.total_red / _sta.time_used,
+                _sta.max_speed,
+                _sta.speed_list,
+                _sta.total_red,
+            )
+        else:
+            return 0, 0, [], 0
