8f4b008a70d389d0f0e36a5a5088e517bcf27cda
Oreomeow <85870818+Oreomeow@users.noreply.github.com>
2022-09-12 09:38:15 +0800

    :fire: Remove unused modules & Rname some modules

--- ssrspeed/threadpool/__init__.py
+++
@@ -1,6 +0,0 @@
-from .abstract_task import AbstractTask
-from .task_list import TASK_LIST
-from .thread_pool import ThreadPool
-from .work_thread import WorkThread
-
-__all__ = ["AbstractTask", "TASK_LIST", "ThreadPool", "WorkThread"]
--- ssrspeed/threadpool/abstract_task.py
+++
@@ -1,7 +0,0 @@
-class AbstractTask(object):
-    def __init__(self, *args, **kwargs):
-        self.__args = args
-        self.__kwargs = kwargs
-
-    def execute(self):
-        raise NotImplementedError
