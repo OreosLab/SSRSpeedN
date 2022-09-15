9c08c8aa1d03f625a7796e650f8a586e99f930e3
Mashiro2000 <2984922017@qq.com>
2022-09-16 07:44:59 +0800

    fix an small bug

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os.path
 
-from setuptools import find_namespace_packages, setup
+from setuptools import find_packages, setup
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 
@@ -12,25 +12,19 @@ setup(
     license="GPL-3.0 License",
     description="A simple tool to test nodes.",
     long_description=open(
-        os.path.join(this_directory, "README.md"), encoding="utf-8"
+        file=os.path.join(this_directory, "README.md"),
+        mode="r",
+        encoding="utf-8"
     ).read(),
     long_description_content_type="text/markdown",
-    packages=find_namespace_packages(include=["ssrspeed.*"]),
+    packages=find_packages(include=["ssrspeed.*"]),
     include_package_data=True,
     python_requires=">=3.8.0",
-    install_requires=[
-        "aiofiles",
-        "aiohttp-socks",
-        "Flask-Cors",
-        "geoip2",
-        "loguru",
-        "pilmoji",
-        "PySocks",
-        "PyYAML",
-        "requests",
-        "selenium==4.4.3",
-        "webdriver-manager",
-    ],
+    install_requires=open(
+        os.path.join(this_directory, "requirements.txt"),
+        mode="r",
+        encoding="utf-8"
+    ).readlines(),
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
