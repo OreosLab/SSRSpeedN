ac36f5c4724b0e96111b3006e5c6674d3d978912
Oreomeow <85870818+Oreomeow@users.noreply.github.com>
2022-09-16 10:23:19 +0800

    :sparkles: Add package & distribute tools

--- setup.py
+++ setup.py
@@ -1,36 +1,34 @@
 import os.path
+import re
 
-from setuptools import find_namespace_packages, setup
+from setuptools import find_packages, setup
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 
+with open(os.path.join(this_directory, "ssrspeed/__init__.py"), mode="r", encoding="utf-8") as v:
+    version = re.findall(r'__version__ = "(.*?)"', v.read())[0]
+
+with open(os.path.join(this_directory, "README.md"), mode="r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open(
+    os.path.join(this_directory, "requirements.txt"), mode="r", encoding="utf-8"
+) as r:
+    install_requires = [i.strip() for i in r]
+
 setup(
     name="ssrspeed",
-    version="1.4.3",
+    version=version,
     keywords=["ssr", "speed", "test"],
     url="https://github.com/OreosLab/SSRSpeedN",
     license="GPL-3.0 License",
     description="A simple tool to test nodes.",
-    long_description=open(
-        os.path.join(this_directory, "README.md"), encoding="utf-8"
-    ).read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_namespace_packages(include=["ssrspeed.*"]),
+    packages=find_packages(include=["ssrspeed.*"]),
     include_package_data=True,
     python_requires=">=3.8.0",
-    install_requires=[
-        "aiofiles",
-        "aiohttp-socks",
-        "Flask-Cors",
-        "geoip2",
-        "loguru",
-        "pilmoji",
-        "PySocks",
-        "PyYAML",
-        "requests",
-        "selenium==4.4.3",
-        "webdriver-manager",
-    ],
+    install_requires=install_requires,
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
