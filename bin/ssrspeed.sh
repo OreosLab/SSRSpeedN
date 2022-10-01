bb37a21239a1eafabed5bf29f6c1cec46a03fed4
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-10-01 21:50:36 +0800

    ✨ V1.0.6 Support hysteria.

--- bin/ssrspeed.sh
+++ bin/ssrspeed.sh
@@ -3,13 +3,13 @@
 # shellcheck disable=SC2015,2034,2089,2090
 
 # 当前脚本版本号，设置工作文件夹
-VERSION=1.0.5
+VERSION=1.0.6
 WORKDIR=SSRSpeedN
 
 E[0]="Language:\n 1. English (default) \n 2. 简体中文"
 C[0]="${E[0]}"
-E[1]="New Features: Support SSRSpeedN V1.5.0"
-C[1]="新特性: 支持 SSRSpeedN V1.5.0"
+E[1]="New Features: Support SSRSpeedN V1.5.1 - test VLESS and hysteria protocols."
+C[1]="新特性: 支持 SSRSpeedN V1.5.1 -  测试 VLESS 和 hysteria "
 E[2]="Speed test and unlocking test is for reference only and does not represent the actual usage, due to network changes, Netflix blocking and ip replacement. Speed test is time-sensitive."
 C[2]="测速及解锁测试仅供参考, 不代表实际使用情况, 由于网络情况变化, Netflix 封锁及 ip 更换, 测速具有时效性。"
 E[3]="Choose:"
@@ -247,12 +247,12 @@ check_ssrspeedn() {
   [ ! -d $WORKDIR ] && sudo git clone https://github.com/Oreomeow/SSRSpeedN
   [ ! -d $WORKDIR/resources/databases ] && sudo mkdir -p $WORKDIR/resources/databases
   [ ! -d $WORKDIR/data ] && sudo mkdir -p $WORKDIR/data
-  if [ ! -e $WORKDIR/resources/clients/xray-core/xray ]; then
+  if [ ! -e $WORKDIR/resources/clients/hysteria/hysteria ]; then
     LATEST=$(sudo wget --no-check-certificate -qO- "https://api.github.com/repos/Oreomeow/SSRSpeedN/releases/latest" | grep tag_name | sed -E 's/.*"([^"]+)".*/\1/' | cut -c 2-)
-    LATEST=${LATEST:-'1.4.6'}
+    LATEST=${LATEST:-'1.5.1'}
     sudo wget --no-check-certificate -O $WORKDIR/resources/$FILE https://github.com/OreosLab/SSRSpeedN/releases/download/v"$LATEST"/$FILE
-    [ ! -e $WORKDIR/resources/$FILE ] && error " $(text 18) " || sudo unzip -d $WORKDIR/resources/clients $WORKDIR/resources/$FILE
-    [ ! -e $WORKDIR/resources/clients/xray-core/xray ] && error " $(text 19) " || sudo rm -f $WORKDIR/resources/$FILE
+    [ ! -e $WORKDIR/resources/$FILE ] && error " $(text 18) " || sudo unzip -nd $WORKDIR/resources/clients $WORKDIR/resources/$FILE
+    [ ! -e $WORKDIR/resources/clients/hysteria/hysteria ] && error " $(text 19) " || sudo rm -f $WORKDIR/resources/$FILE
   fi
   sudo chmod -R +x $WORKDIR
   cd $WORKDIR || exit 1
